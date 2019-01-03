c1ba141dec219917a9429179aefe646deb6c9b22
mmajam <majiku@seznam.cz>
2019-01-03 23:31:57 +0100

    basic structure of the project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* Whole configuration done using 
+https://hackernoon.com/a-tale-of-webpack-4-and-how-to-finally-configure-it-in-the-right-way-4e94c8e7e5c1 */
+
+
+import "./sass/main.scss";
+
+console.log("hello, worldddd!");
